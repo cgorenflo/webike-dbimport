3734458325133c633e8471ac8ce9c8509f5535a3
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2016-11-17 21:27:25 -0500

    Data import working for new log file format

---
+++ iss4e/webike/db/csv_formatters.py
@@ -0,0 +1,39 @@
+import ast
+import csv
+from abc import ABCMeta, abstractmethod
+
+
+class Formatter(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def format(self, reader: csv.DictReader): pass
+
+
+class V1Formatter(Formatter):
+    pass
+
+
+class V2Formatter(Formatter):
+    pass
+
+
+class V3Formatter(Formatter):
+    def format(self, reader: csv.DictReader) -> dict:
+        """
+        :param reader: log file data
+        :returns formatted data
+        """
+        return {"points": [{"measurement": "sensor_data",
+                            "tags": {"imei": row.pop("IMEI"),
+                                     "code_version": ast.literal_eval(row.pop("code_version"))},
+                            "time": row.pop("timestamp"),
+                            "fields": self._get_fields_with_correct_data_type(row)
+                            } for row in reader if self._filter_for_correct_log_format(row)]}
+
+    def _get_fields_with_correct_data_type(self, row: dict) -> dict:
+        return dict([key, ast.literal_eval(value.title())] for key, value in row.items() if value)
+
+    def _filter_for_correct_log_format(self, row: dict) -> bool:
+        print(row)
+        return "IMEI" in row.keys()
---
+++ iss4e/webike/db/import_data.py
@@ -0,0 +1,66 @@
+import os
+import re
+import sys
+from typing import Iterator, Tuple
+
+import iss4e.db.influxdb as influxdb
+from iss4e.util.config import load_config
+
+from iss4e.webike.db.csv_formatters import *
+
+config = load_config()
+print(config)
+
+
+def import_data(version: str = "v3"):
+    csv_formatters = {"v1": V1Formatter(), "v2": V2Formatter(), "v3": V3Formatter()}
+
+    log_file_paths = _get_log_file_paths()
+    logs = _read_logs(log_file_paths, csv_formatters[version])
+    _insert_into_db_and_archive_logs(logs)
+
+
+def _get_log_file_paths() -> Iterator[Tuple[str, str]]:
+    """
+    :returns an iterator over directories and log file names
+    """
+    directory_regex_pattern = config["webike.imei_regex"]
+    file_regex_pattern = config["webike.imei_regex"]
+    home, dirs, _ = next(os.walk(os.path.expanduser("~")))
+    for directory in dirs:
+        if re.fullmatch(directory_regex_pattern, directory):
+            current_directory, _, files = next(os.walk(os.path.join(home, directory)))
+            for file in files:
+                if re.fullmatch(file_regex_pattern, file):
+                    yield current_directory, file
+
+
+def _read_logs(log_file_paths: Iterator[Tuple[str, str]], formatter: Formatter) -> Iterator[Tuple[str, str, dict]]:
+    """
+    :param log_file_paths: An iterator over directories and log file names
+    :returns an iterator over directories, log file names of their data
+    """
+    for directory, file_name in log_file_paths:
+        with open(os.path.join(directory, file_name)) as csv_file:
+            reader = csv.DictReader(csv_file)
+            data = formatter.format(reader)
+            if data["points"]:
+                yield directory, file_name, data
+
+
+def _insert_into_db_and_archive_logs(path_and_data: Iterator[Tuple[str, str, dict]]):
+    """
+    :param path_and_data: an iterator over directories, log file names of their data
+    """
+    with influxdb.connect(config["webike.influx"]) as client:
+        for directory, filename, data in path_and_data:
+            client.write(data)
+            _archive_log(directory, filename)
+
+
+def _archive_log(directory: str, filename: str):
+    os.rename(os.path.join(directory, filename), os.path.join(directory, config["webike.archive"], filename))
+
+
+if __name__ == "__main__":
+    sys.exit(import_data())
